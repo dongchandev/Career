66f112185677b92aebd125ab47ae48ee2be22d59
김동찬 <129833370+dongchandev@users.noreply.github.com>
2023-08-27 01:25:39 +0900

    LAST

--- src/main/java/com/hackathon/career/domain/chatGpt/ChatGptService.java
+++ src/main/java/com/hackathon/career/domain/chatGpt/ChatGptService.java
@@ -11,12 +11,9 @@ public class ChatGptService{
     private final ChatgptService chatgptService;
     public String getChatResponse() {
 
-        String prompt="I want you to act as a career counselor. Just tell me what I asked for. Your task is to fill out a questionnaire to help people find the best job for them based on their interests and experience. You should also conduct research into the various options available and the job market trends in different industries. My first request is a five-question multiple-choice questionnaire to recommend the most suitable job for the respondent. \n" +
-                "{Question 1: What kind of activities do you most enjoy doing in your spare time?\n" +
-                "A) Analysing data and solving puzzles,\n" +
-                "B) creating artwork, writing, and playing musical instruments,\n" +
-                "C) being helpful and interacting with others,\n" +
-                "d) designing or making things} in JSON format.";
-        return chatgptService.sendMessage(prompt);
+        String prompt="I want you to act as a career counselor. Just tell me what I asked for. Your task is to fill out a questionnaire to help people find the best job for them based on their interests and experience. You should also conduct research into the various options available and the job market trends in different industries. My first request is \"Please provide a 5-question multiple-choice questionnaire in Korean in JSON format to recommend the best job for the respondent.\"";
+
+
+            return chatgptService.sendMessage(prompt);
     }
 }
